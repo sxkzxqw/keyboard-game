015bd803eb911d28931db4aca941025207a391af
sxkzxqw <marsel.vagizov@bk.ru>
2023-02-10 02:33:06 +0300

    added game layout

--- js/script.js
+++ js/script.js
@@ -2,6 +2,83 @@
 
 //keyboard
 
+// keyup functionality
+document.addEventListener('keyup', function(e) {
+    handleKeyUp(e);
+});
+
+function handleKeyUp(e) {
+    switch (e.keyCode) {
+        case 188:
+            document.querySelector('.ldotbtn').classList.remove('key_type_active');
+        break;
+        case 190:
+            document.querySelector('.dotbtn').classList.remove('key_type_active');
+        break;
+        case 191:
+            document.querySelector('.quest').classList.remove('key_type_active');
+        break;
+        case 32:
+            document.querySelector('.space').classList.remove('key_type_active');
+        break;
+        case 186:
+            document.querySelector('.doubledot').classList.remove('key_type_active');
+        break;
+        case 222:
+            document.querySelector('.updot').classList.remove('key_type_active');
+        break;
+        case 27:
+            document.querySelector('.esc').classList.remove('key_type_active');
+        break;
+        case 49:
+            document.querySelector('.onebtn').classList.remove('key_type_active');
+        break;
+        case 50:
+            document.querySelector('.twobtn').classList.remove('key_type_active');
+        break;
+        case 51:
+            document.querySelector('.threebtn').classList.remove('key_type_active');
+        break;
+        case 52:
+            document.querySelector('.fourbtn').classList.remove('key_type_active');
+        break;
+        case 53:
+            document.querySelector('.fivebtn').classList.remove('key_type_active');
+        break;
+        case 54:
+            document.querySelector('.sixbtn').classList.remove('key_type_active');
+        break;
+        case 55:
+            document.querySelector('.sevenbtn').classList.remove('key_type_active');
+        break;
+        case 56:
+            document.querySelector('.eightbtn').classList.remove('key_type_active');
+        break;
+        case 57:
+            document.querySelector('.ninebtn').classList.remove('key_type_active');
+        break;
+        case 48:
+            document.querySelector('.zerobtn').classList.remove('key_type_active');
+        break;
+        case 219:
+            document.querySelector('.backbtn').classList.remove('key_type_active');
+        break;
+        case 221:
+            document.querySelector('.backotherbtn').classList.remove('key_type_active');
+        break;
+        case 220:
+            document.querySelector('.backanotherbtn').classList.remove('key_type_active');
+        break;
+        case 187:
+            document.querySelector('.ravnobtn').classList.remove('key_type_active');
+        break;
+        default:
+            let key = `${e.key}btn`;
+            let keyLow = key.toLowerCase();
+            document.querySelector(`.${keyLow}`).classList.remove('key_type_active');
+        break;
+    }
+}
 
 //keydown functionality
 document.addEventListener("keydown", function (e) {
@@ -77,84 +154,6 @@ function handleKeyDown(e) {
             let key = `${e.key}btn`;
             let keyLow = key.toLowerCase();
             document.querySelector(`.${keyLow}`).classList.add('key_type_active');
-        break;
-    }
-}
-
-// keyup functionality
-document.addEventListener('keyup', function(e) {
-    handleKeyUp(e);
-});
-
-function handleKeyUp(e) {
-    switch (e.keyCode) {
-        case 188:
-            document.querySelector('.ldotbtn').classList.remove('key_type_active');
-        break;
-        case 190:
-            document.querySelector('.dotbtn').classList.remove('key_type_active');
-        break;
-        case 191:
-            document.querySelector('.quest').classList.remove('key_type_active');
-        break;
-        case 32:
-            document.querySelector('.space').classList.remove('key_type_active');
-        break;
-        case 186:
-            document.querySelector('.doubledot').classList.remove('key_type_active');
-        break;
-        case 222:
-            document.querySelector('.updot').classList.remove('key_type_active');
-        break;
-        case 27:
-            document.querySelector('.esc').classList.remove('key_type_active');
-        break;
-        case 49:
-            document.querySelector('.onebtn').classList.remove('key_type_active');
-        break;
-        case 50:
-            document.querySelector('.twobtn').classList.remove('key_type_active');
-        break;
-        case 51:
-            document.querySelector('.threebtn').classList.remove('key_type_active');
-        break;
-        case 52:
-            document.querySelector('.fourbtn').classList.remove('key_type_active');
-        break;
-        case 53:
-            document.querySelector('.fivebtn').classList.remove('key_type_active');
-        break;
-        case 54:
-            document.querySelector('.sixbtn').classList.remove('key_type_active');
-        break;
-        case 55:
-            document.querySelector('.sevenbtn').classList.remove('key_type_active');
-        break;
-        case 56:
-            document.querySelector('.eightbtn').classList.remove('key_type_active');
-        break;
-        case 57:
-            document.querySelector('.ninebtn').classList.remove('key_type_active');
-        break;
-        case 48:
-            document.querySelector('.zerobtn').classList.remove('key_type_active');
-        break;
-        case 219:
-            document.querySelector('.backbtn').classList.remove('key_type_active');
-        break;
-        case 221:
-            document.querySelector('.backotherbtn').classList.remove('key_type_active');
-        break;
-        case 220:
-            document.querySelector('.backanotherbtn').classList.remove('key_type_active');
-        break;
-        case 187:
-            document.querySelector('.ravnobtn').classList.remove('key_type_active');
-        break;
-        default:
-            let key = `${e.key}btn`;
-            let keyLow = key.toLowerCase();
-            document.querySelector(`.${keyLow}`).classList.remove('key_type_active');
         break;
     }
 }
@@ -260,3 +259,51 @@ handleSwitchThemeLight();
 //keyboard
 
 //---------------------------------------------------------------------------------------
+
+
+
+//---------------------------------------------------------------------------------------
+
+//game logic
+
+
+//removing empty strokes in array
+/* const arrayOfPlaceHolderWords = word.filter(function (word) { 
+    return word.length > 0;
+}); */
+
+const placeHolderText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam explicabo veritatis, sapiente doloribus placeat reprehenderit 
+quasi voluptas ipsam quos perferendis laudantium expedita quidem minima cum temporibus consequuntur praesentium tenetur quas?
+Consequatur quae deleniti voluptate dolore consectetur! Voluptatum vel accusamus aut. Iure fugit nisi reiciendis quos repudiandae 
+exercitationem adipisci sed, tenetur deserunt, rem voluptas odio, expedita laborum sint culpa ex cumque?
+At illo quibusdam ratione eos blanditiis quisquam eius doloribus molestiae ipsam, ea temporibus earum laudantium? Magnam similique 
+reprehenderit placeat, provident quidem aperiam impedit illum deserunt enim voluptate laborum, id rem.
+Dolor perspiciatis itaque optio? Enim animi consequatur, totam repellendus recusandae, provident accusamus accusantium placeat, 
+sequi modi optio voluptas repudiandae! Modi animi cum sint saepe ea, consequuntur repellat pariatur ducimus tempore.
+Nam, illo, eum accusamus inventore temporibus voluptate obcaecati, impedit doloremque ipsam et dolorum illum doloribus fuga. 
+Ab autem at nostrum, repudiandae, aspernatur sed ullam, nulla optio facilis reiciendis obcaecati iusto.`;
+
+const text = placeHolderText.split(' ');
+const templateForEachWord = document.querySelector('#words').content;
+const templateSpan = templateForEachWord.querySelector('.word-place');
+const placeHolder = document.querySelector('.game__placeholder');
+
+/* function addWordToSpan() {
+    text.forEach(function (word) {
+        templateForEachWord.cloneNode(true);
+        templateForEachWord.querySelector('.word-place').textContent = word;
+        placeHolder.append(templateForEachWord);
+    });
+}
+
+addWordToSpan(); */
+
+function addWordToSpan(word) {
+        const newSpan = templateSpan.cloneNode(true);
+        newSpan.textContent = `${word} `;
+        placeHolder.append(newSpan);
+}
+
+text.forEach(function(text) {
+    addWordToSpan(text);
+});
