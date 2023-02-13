3efa4541c63c4cdfe44d5b196fcf856da46c86e9
sxkzxqw <marsel.vagizov@bk.ru>
2023-02-13 09:19:49 +0300

    production version

--- src/index.js
+++ src/index.js
@@ -1,166 +1,60 @@
 import './pages/index.css';
 import { fishText } from './components/fish-text';
-console.log(fishText);
 let randomText = fishText.getWords({count: 180, dataType: 'string', lang: 'eng', repeat: true});
 //----------------------------------------------------------------------------
 
 //keyboard
 
-// keyup functionality
-document.addEventListener('keyup', function(e) {
-    handleKeyUp(e);
-});
-
-function handleKeyUp(e) {
-    switch (e.keyCode) {
-        case 188:
-            document.querySelector('.ldotbtn').classList.remove('key_type_active');
-        break;
-        case 190:
-            document.querySelector('.dotbtn').classList.remove('key_type_active');
-        break;
-        case 191:
-            document.querySelector('.quest').classList.remove('key_type_active');
-        break;
-        case 32:
-            document.querySelector('.space').classList.remove('key_type_active');
-        break;
-        case 186:
-            document.querySelector('.doubledot').classList.remove('key_type_active');
-        break;
-        case 222:
-            document.querySelector('.updot').classList.remove('key_type_active');
-        break;
-        case 27:
-            document.querySelector('.esc').classList.remove('key_type_active');
-        break;
-        case 49:
-            document.querySelector('.onebtn').classList.remove('key_type_active');
-        break;
-        case 50:
-            document.querySelector('.twobtn').classList.remove('key_type_active');
-        break;
-        case 51:
-            document.querySelector('.threebtn').classList.remove('key_type_active');
-        break;
-        case 52:
-            document.querySelector('.fourbtn').classList.remove('key_type_active');
-        break;
-        case 53:
-            document.querySelector('.fivebtn').classList.remove('key_type_active');
-        break;
-        case 54:
-            document.querySelector('.sixbtn').classList.remove('key_type_active');
-        break;
-        case 55:
-            document.querySelector('.sevenbtn').classList.remove('key_type_active');
-        break;
-        case 56:
-            document.querySelector('.eightbtn').classList.remove('key_type_active');
-        break;
-        case 57:
-            document.querySelector('.ninebtn').classList.remove('key_type_active');
-        break;
-        case 48:
-            document.querySelector('.zerobtn').classList.remove('key_type_active');
-        break;
-        case 219:
-            document.querySelector('.backbtn').classList.remove('key_type_active');
-        break;
-        case 221:
-            document.querySelector('.backotherbtn').classList.remove('key_type_active');
-        break;
-        case 220:
-            document.querySelector('.backanotherbtn').classList.remove('key_type_active');
-        break;
-        case 187:
-            document.querySelector('.ravnobtn').classList.remove('key_type_active');
-        break;
-        default:
-            let key = `${e.key}btn`;
-            let keyLow = key.toLowerCase();
-            document.querySelector(`.${keyLow}`).classList.remove('key_type_active');
-        break;
+//keys functionality
+const keys = {
+    188: '.ldotbtn',
+    190: '.dotbtn',
+    191: '.quest',
+    32: '.space',
+    186: '.doubledot',
+    222: '.updot',
+    27: '.esc',
+    49: '.onebtn',
+    50: '.twobtn',
+    51: '.threebtn',
+    52: '.fourbtn',
+    53: '.fivebtn',
+    54: '.sixbtn',
+    55: '.sevenbtn',
+    56: '.eightbtn',
+    57: '.ninebtn',
+    48: '.zerobtn',
+    219: '.backbtn',
+    221: '.backotherbtn',
+    220: '.backanotherbtn',
+    187: '.ravnobtn',
+  };
+  
+  document.addEventListener('keydown', handleKeyDown);
+  document.addEventListener('keyup', handleKeyUp);
+  
+  function handleKeyDown(e) {
+    const key = keys[e.keyCode];
+    if (key) {
+      document.querySelector(key).classList.add('key_type_active');
+    } else {
+      const key = `${e.key}btn`;
+      const keyLow = key.toLowerCase();
+      document.querySelector(`.${keyLow}`).classList.add('key_type_active');
     }
-}
-
-//keydown functionality
-document.addEventListener("keydown", function (e) {
-    handleKeyDown(e);
-});
-
-function handleKeyDown(e) {
-    switch (e.keyCode) {
-        case 188:
-            document.querySelector('.ldotbtn').classList.add('key_type_active');
-        break;
-        case 190:
-            document.querySelector('.dotbtn').classList.add('key_type_active');
-        break;
-        case 191:
-            document.querySelector('.quest').classList.add('key_type_active');
-        break;
-        case 32:
-            document.querySelector('.space').classList.add('key_type_active');
-        break;
-        case 186:
-            document.querySelector('.doubledot').classList.add('key_type_active');
-        break;
-        case 222:
-            document.querySelector('.updot').classList.add('key_type_active');
-        break;
-        case 27:
-            document.querySelector('.esc').classList.add('key_type_active');
-        break;
-        case 49:
-            document.querySelector('.onebtn').classList.add('key_type_active');
-        break;
-        case 50:
-            document.querySelector('.twobtn').classList.add('key_type_active');
-        break;
-        case 51:
-            document.querySelector('.threebtn').classList.add('key_type_active');
-        break;
-        case 52:
-            document.querySelector('.fourbtn').classList.add('key_type_active');
-        break;
-        case 53:
-            document.querySelector('.fivebtn').classList.add('key_type_active');
-        break;
-        case 54:
-            document.querySelector('.sixbtn').classList.add('key_type_active');
-        break;
-        case 55:
-            document.querySelector('.sevenbtn').classList.add('key_type_active');
-        break;
-        case 56:
-            document.querySelector('.eightbtn').classList.add('key_type_active');
-        break;
-        case 57:
-            document.querySelector('.ninebtn').classList.add('key_type_active');
-        break;
-        case 48:
-            document.querySelector('.zerobtn').classList.add('key_type_active');
-        break;
-        case 219:
-            document.querySelector('.backbtn').classList.add('key_type_active');
-        break;
-        case 221:
-            document.querySelector('.backotherbtn').classList.add('key_type_active');
-        break;
-        case 220:
-            document.querySelector('.backanotherbtn').classList.add('key_type_active');
-        break;
-        case 187:
-            document.querySelector('.ravnobtn').classList.add('key_type_active');
-        break;
-        default:
-            let key = `${e.key}btn`;
-            let keyLow = key.toLowerCase();
-            document.querySelector(`.${keyLow}`).classList.add('key_type_active');
-        break;
+  }
+  
+  function handleKeyUp(e) {
+    const key = keys[e.keyCode];
+    if (key) {
+      document.querySelector(key).classList.remove('key_type_active');
+    } else {
+      const key = `${e.key}btn`;
+      const keyLow = key.toLowerCase();
+      document.querySelector(`.${keyLow}`).classList.remove('key_type_active');
     }
-}
+  }
+  
 
 
 // switch background color
@@ -271,57 +165,51 @@ handleSwitchThemeLight();
 //game logic
 
 
-
 //text validation
 const gameTemplate = document.querySelector('#game-template').content;
-const placeHolderText = `${randomText}`;
+let placeHolderText = `${randomText}`;
 
-const text = placeHolderText.split(' ');
+//countdown selectors 
+const gameoverTemplate = document.querySelector('#gameover').content;
+const gameoverContainer = gameoverTemplate.querySelector('.gameover__container');
+const gameoverContentConrainer = gameoverTemplate.querySelector('.gameover__content-container');
+const gameoverText = gameoverTemplate.querySelector('.gameover__result');
+const timerTemplate = document.querySelector('#timer-template').content;
+const countDownText = timerTemplate.querySelector('.timer__time-counter');
+const timerContent = timerTemplate.querySelector('.timer');
+const game = gameTemplate.querySelector('.game');
+
+let text = placeHolderText.split(' ');
 const templateForEachWord = document.querySelector('#words').content;
 const templateSpan = templateForEachWord.querySelector('.word-place');
-const placeHolder = gameTemplate.querySelector('.game__placeholder');
+let placeHolder = gameTemplate.querySelector('.game__placeholder');
 const gameText = gameTemplate.querySelector('.game__text');
 
 //using cycle to add a number of word to classlist
-for (let i = 0; i < text.length; i++) { 
-    const newSpan = templateSpan.cloneNode(true);
-    newSpan.classList.add(`wordplace${i}`);
-    newSpan.textContent = `${text[i]} `;
-    placeHolder.append(newSpan);
+function textAppending (text) {
+    for (let i = 0; i < text.length; i++) { 
+        const newSpan = templateSpan.cloneNode(true);
+        newSpan.classList.add(`wordplace${i}`);
+        newSpan.textContent = `${text[i]} `;
+        placeHolder.append(newSpan);
+    }
 }
 
-const inputText = gameTemplate.querySelector('.game__text');
+textAppending(text);
 
-/* inputText.addEventListener('input', function(event) {
-    checkTextAccordance(event);
-}); */
-
-/* const everySymbolInText = placeHolderText.split('');
-function checkTextAccordance(event) {
-    let inputLength = inputText.value.length;
-    let currentInputLength = inputLength - 1;
-    let inputSymbol = event.data;
-    if (inputSymbol == everySymbolInText[currentInputLength]) {
-        inputText.classList.remove('game__text_type_incorrect');
-        inputText.classList.add('game__text_type_correct');
-    } else if (inputSymbol != everySymbolInText[currentInputLength]) {
-        inputText.classList.remove('game__text_type_correct');
-        inputText.classList.add('game__text_type_incorrect');
-    }
-} */
-
-
-
-let counterOfWords = -1;
 let counterOfCorrectWords = 0;
-const configForFindWord = `.wordplace`;
-gameText.addEventListener('keydown', function(event) {
+let counterOfWords = -1;
+
+function gameValidation(gameTemplate, placeHolder) {
+    const inputText = gameTemplate.querySelector('.game__text');
+    const configForFindWord = `.wordplace`;
+    function handleKeydownEvent(event) {
     if (event.keyCode === 32) {
         const inputTextArray = inputText.value.split(' ');
         counterOfWords++;
         const currentWord = inputTextArray[inputTextArray.length - 1];
         const correctWordClass = placeHolder.querySelector(`${configForFindWord}${counterOfWords}`);
-        const correctWord = correctWordClass.textContent.trim();
+        let correctWord = correctWordClass.textContent.trim();
         if (currentWord == correctWord) {
             correctWordClass.classList.add('game__text_type_correct');
             counterOfCorrectWords++;
@@ -339,7 +227,7 @@ gameText.addEventListener('keydown', function(event) {
         getCurrentText();
         /* console.log(correctWordClass.offsetLeft);
         console.log(correctWordClass.offsetTop); */
-    } else if (event.keyCode === 8) {
+        } else if (event.keyCode === 8) {
         function getCurrentTextLength(event) {
             let currentText = '';
             for (let i = counterOfWords + 1; i > 0; i--) {
@@ -353,8 +241,24 @@ gameText.addEventListener('keydown', function(event) {
         }
         getCurrentTextLength(event);
     }
-});
+    if (placeHolderText.length <= gameText.value.length || countDownText.textContent == '0:01') {
+        gameText.removeEventListener('keydown', handleKeydownEvent);
+    }
+}
+function checkTimer() {
+    setInterval(function() {
+      console.log(countDownText.textContent);
+      if (placeHolderText.length <= gameText.value.length || countDownText.textContent == '0:01') {
+        gameText.removeEventListener('keydown', handleKeydownEvent);
+    }
+    }, 1000);
+  }
+  
+  checkTimer();
+    gameText.addEventListener('keydown', handleKeydownEvent);
+}
 
+gameValidation(gameTemplate, placeHolder);
 
 //start game functionality
 const startGameButton = document.querySelector('.start-game-button');
@@ -362,22 +266,22 @@ const buttonContainer = document.querySelector('.button-container');
 const content = document.querySelector('.content');
 const keyboardSuggestTemplate = document.querySelector('#keyboard-suggest').content;
 const keyboardSuggestContent = keyboardSuggestTemplate.querySelector('.keyboard__suggest');
-const timerTemplate = document.querySelector('#timer-template').content;
 
 
 startGameButton.addEventListener('click', () => {
     startGameButton.setAttribute('style', 'animation: scaling 0.4s ease; animation-delay: 0s;');
-    addGame();
+    addGame(startGameButton, gameTemplate, timerTemplate, buttonContainer);
     document.addEventListener('keydown', addCountDown);
 });
 
 
-function addGame() {
+function addGame(firstRemove, prepend, append, secondRemove) {
+    counterOfWords = -1;
     setTimeout(() => {
-        startGameButton.remove();
-        buttonContainer.remove();
-        content.prepend(gameTemplate);
-        content.append(timerTemplate);
+        firstRemove.remove();
+        secondRemove.remove();
+        content.prepend(prepend);
+        content.append(append);
         gameText.focus();
         content.append(keyboardSuggestTemplate);
         gameStartSuggestionShow();
@@ -403,29 +307,41 @@ function gameStart() {
 }
 
 //countdown functionality
-const gameoverTemplate = document.querySelector('#gameover').content;
-const gameoverText = gameoverTemplate.querySelector('.gameover__result');
-let time = 58;
-const countDownText = timerTemplate.querySelector('.timer__time-counter');
-const timerContent = timerTemplate.querySelector('.timer');
-const game = gameTemplate.querySelector('.game');
-const gameRestartButton = gameTemplate.querySelector('.gameover__button');
 
-function addCountDown(e) {
+let time = 58;
+
+function addCountDown() {
     countDownText.textContent = `0:59`;
     setInterval(updateCountDown, 1000);
     document.removeEventListener('keydown', addCountDown);
 }
 
-function updateCountDown(e) {
+let inputLength = gameText.value.length - 1;
+
+function updateCountDown() {
     let minutes = Math.floor(time / 60);
     let seconds = time % 60;
     seconds = seconds < 10 ? "0" + seconds:seconds;
     countDownText.textContent = `${minutes}:${seconds}`;
     time--;
+    if (countDownText.textContent == '0:00' || placeHolderText.length <= inputLength) {
+        timerContent.setAttribute('style', 'animation: scaling 0.4s ease');
+        game.setAttribute('style', 'animation: scaling 0.4s ease');
+        gameoverText.textContent = `Your result: ${counterOfCorrectWords} words / per min`;
+        countDownText.textContent = '';
+        inputLength = 0;
+        setTimeout(() => {
+            gameoverContentConrainer.setAttribute('style', 'animation: scaling-reverse 0.4s ease;');
+            timerContent.remove();
+            game.remove();
+            content.prepend(gameoverContentConrainer);
+            document.removeEventListener('keydown', addCountDown);
+        }, 400);
+    }
 }
 
-function checkGameOver() {
+//gameover functionality
+function checkGameOver(gameText) {
     gameText.addEventListener('input', checkInputForGameOver);
 }
 
@@ -435,11 +351,53 @@ function checkInputForGameOver() {
         game.setAttribute('style', 'animation: scaling 0.4s ease');
         gameoverText.textContent = `Your result: ${counterOfCorrectWords} words / per min`;
         setTimeout(() => {
+            console.log('aa');
+            gameoverContentConrainer.setAttribute('style', 'animation: scaling-reverse 0.4s ease;');
             timerContent.remove();
             game.remove();
-            content.prepend(gameoverTemplate);
+            content.prepend(gameoverContentConrainer);
+            document.removeEventListener('keydown', addCountDown);
         }, 400);
     }
 }
 
-checkGameOver();
+checkGameOver(gameText);
+
+
+//restart game functionality
+const restartGameButton = gameoverTemplate.querySelector('.gameover__button');
+restartGameButton.addEventListener('click', function () {
+    placeHolder.replaceChildren();
+    gameoverContentConrainer.setAttribute('style', 'animation: scaling 0.4s ease;');
+    setTimeout(() => {
+        gameoverContentConrainer.remove();
+        timerContent.setAttribute('style', 'animation: scaling-reverse 0.4s ease;');
+        keyboardSuggestContent.setAttribute('style', 'animation: scaling-reverse 0.4s ease');
+        inputLength = gameText.value.length - 1;
+    }, 400);
+    game.setAttribute('style', 'animation: scaling-reverse 0.4s ease;');
+    restartGame();
+    addGame(gameoverContainer, game, '', gameoverContainer);
+    content.prepend(keyboardSuggestContent);
+    let gameTextRestart = game.querySelector('.game__text');
+    checkGameOver(gameTextRestart);
+});
+
+function restartGame() {
+    gameText.value = '';
+    placeHolderText = fishText.getWords({count: 180, dataType: 'string', lang: 'eng', repeat: true});
+    text = placeHolderText.split(' ');
+    counterOfWords = -1;
+    counterOfCorrectWords = 0;
+    textAppending(text);
+    placeHolder = game.querySelector('.game__placeholder');
+    gameValidation(game, placeHolder);
+    document.addEventListener('keydown', addTimerAfterRestart);
+}
+
+function addTimerAfterRestart() {
+    countDownText.textContent = `1:00`;
+    time = 59;
+    content.append(timerContent);
+    document.removeEventListener('keydown', addTimerAfterRestart);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -50,4 +50,4 @@ module.exports = {
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin()
   ]
-} 
+};
